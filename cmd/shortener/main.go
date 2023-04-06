56288c50b5017611b7f959c968ad3867ae155c3f
Your Name <you@example.com>
2023-04-06 13:11:26 +0300

    iter1. Сделали сервис хранение коротких ссылок в памяти.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,53 +1,196 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
-func mainPage(res http.ResponseWriter, req *http.Request) {
+func getUrlRequest(req *http.Request) string {
+	url := req.Host + req.URL.Path
+	return url
+}
 
-	if req.Method == http.MethodPost {
+func getModuleError(textError string) error {
+	textModuleError := "go-url-shortener: " + textError
+	return errors.New(textModuleError)
+}
 
-		/*
-			var xhr = new XMLHttpRequest();
-			var body = 'name=ttttttttttt';
-			xhr.open("POST", '/', true);
-			xhr.setRequestHeader('Content-Type', 'text/plain');
-			//xhr.onreadystatechange = ...;
-			xhr.send(body);
-		*/
+func getProtocolHttp() string {
+	return "http"
+}
 
-		res.WriteHeader(http.StatusCreated)
-		res.Header().Set("Content-Type", "text/plain")
+func getUrlService(req *http.Request) string {
+	return getProtocolHttp() + "://" + req.Host
+}
 
-		/*
-				Host        string    // host or host:port
-			Path        string    // path (relative paths may omit leading slash)
-		*/
-
-		//fmt.Printf("ddddd %+v", req.URL)
-
-		dataBody := req.Body
-		result := make([]byte, 10)
-		dataBody.Read(result)
-		fmt.Printf("wwwww %+v", result)
-		fmt.Println(string(result))
-
-		res.Write([]byte("EwHXdJfB"))
-		return
-	} else {
-		res.Write([]byte("Что-то не то!"))
-		return
+func NewDataShortLink() DataShortLink {
+	return DataShortLink{
+		Data:            make(map[string]string),
+		lengthShortLink: 8,
 	}
 }
 
+type DataShortLink struct {
+	Data            map[string]string
+	lengthShortLink int
+}
+
+func (store DataShortLink) SetLength(length int) {
+	store.lengthShortLink = length
+}
+
+func (store DataShortLink) getRandString(length int) string {
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	result := b.String()
+	return result
+}
+
+func (store DataShortLink) getShortLinkByUrl(fullUrl string) (shortLink string, err error) {
+
+	for short, urlForShort := range store.Data {
+		if fullUrl == urlForShort {
+			shortLink = short
+			break
+		}
+	}
+
+	if shortLink != "" {
+		return
+	} else {
+		//такой url еще не приходил, генерируем новую ссылку
+		lengthShort := store.lengthShortLink
+		shortLink = store.getRandString(lengthShort)
+		store.Data[shortLink] = fullUrl
+
+		// err = getModuleError("Не сформировать короткую ссылку")
+	}
+	return
+}
+func (store DataShortLink) getFullLinkByShort(shortLink string) (fullUrl string, err error) {
+
+	fullUrl, ok := store.Data[shortLink]
+	if !ok {
+		// должны показать ошибку
+		err = getModuleError("Короткая ссылка " + shortLink + " не зарегистрирована")
+	}
+	return
+}
+
+func mainPage(res http.ResponseWriter, req *http.Request) {
+
+	if req.URL.Path == "/" {
+
+		// генерация короткой ссылки
+		if req.Method == http.MethodPost {
+			// получаем тело из запроса и проводим его к строке
+			dataBody := req.Body
+			lenBody := req.ContentLength
+			result := make([]byte, lenBody)
+			dataBody.Read(result)
+			urlFull := string(result)
+			urlFull = strings.TrimSpace(urlFull)
+			shortLink, err := dataShortLink.getShortLinkByUrl(urlFull)
+			fmt.Println(urlFull)
+			fmt.Printf("%+v", dataShortLink)
+			if err != nil {
+				strError := err.Error()
+				fmt.Println("Ошибка: " + strError)
+
+				res.Header().Set("Content-Type", "text/plain; charset=8")
+				res.WriteHeader(http.StatusBadRequest)
+				res.Write([]byte(strError))
+
+			} else {
+				strResult := getUrlService(req) + "/" + shortLink
+
+				lenResult := len(strResult)
+				strLenResult := fmt.Sprintf("%d", lenResult)
+				res.Header().Set("Content-Length", strLenResult)
+				res.Header().Set("Content-Type", "text/plain; charset=8")
+				res.WriteHeader(http.StatusCreated)
+
+				bytesResult := []byte(strResult)
+				res.Write(bytesResult)
+			}
+
+		} else {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("Вызываемый адрес не существует"))
+		}
+
+	} else {
+
+		// мы хотим получить тут адреса типа /....
+		re, _ := regexp.Compile(`^/([^/]+)$`)
+		resFind := re.FindAllStringSubmatch(req.URL.Path, -1)
+		if len(resFind) > 0 && len(resFind[0]) > 0 {
+
+			// получение полной ссылки из короткой
+			if req.Method == http.MethodGet {
+
+				shortLink := resFind[0][1]
+				shortLink = strings.TrimSpace(shortLink)
+				fmt.Println("Короткая ссылка: " + shortLink)
+
+				fullLink, err := dataShortLink.getFullLinkByShort(shortLink)
+				fmt.Printf("%+v", dataShortLink)
+				if err != nil {
+					strErr := err.Error()
+
+					res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+					res.WriteHeader(http.StatusBadRequest)
+					res.Write([]byte(strErr))
+				} else {
+					res.Header().Set("Location", fullLink)
+					res.WriteHeader(http.StatusTemporaryRedirect)
+				}
+			} else {
+				res.WriteHeader(http.StatusBadRequest)
+				res.Write([]byte("Вызываемый адрес не существует"))
+			}
+
+		} else {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("Вызываемый адрес не существует"))
+		}
+
+	}
+}
+
+// инициализируем хранилище ссылок
+var dataShortLink = NewDataShortLink()
+
 func main() {
+
 	muxApp := http.NewServeMux()
 	muxApp.HandleFunc(`/`, mainPage)
-
 	err := http.ListenAndServe(`:8080`, muxApp)
 	if err != nil {
 		panic(err)
 	}
 }
+
+/*
+var xhr = new XMLHttpRequest();
+var body = 'https://practicum.yandex.ru/';
+xhr.open("POST", '/', true);
+xhr.setRequestHeader('Content-Type', 'text/plain');
+//xhr.onreadystatechange = ...;
+xhr.send(body);
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", '/MIy3I6N4', true);
+*/
+
+// shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
